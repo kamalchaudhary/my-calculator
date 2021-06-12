93dbb5441d673b773c3ba4e3e997367f3982f51d
chaudharykbc <kamalbchaudhary@yahoo.com>
2021-06-12 10:25:43 -0400

    Unit test case of addition

--- CalcTest/UnitTest1.cs
+++ CalcTest/UnitTest1.cs
@@ -18,7 +18,24 @@ namespace CalcTest
             double res = calc.GetAddition(); //act
             Assert.AreEqual(res, 9); //assert
         }
-
+        [Test]
+        public void Addition_of_two_integers_1()
+        {
+            double a = 500, b = 400;//arrange
+            calc.SetFirstNumber(a);//arrange
+            calc.SetSecondNumber(b);//arrange
+            double res = calc.GetAddition(); //act
+            Assert.AreEqual(res, 900); //assert
+        }
+        [Test]
+        public void Addition_of_two_integers_2()
+        {
+            double a = 15, b = 14;//arrange
+            calc.SetFirstNumber(a);//arrange
+            calc.SetSecondNumber(b);//arrange
+            double res = calc.GetAddition(); //act
+            Assert.AreEqual(res, 29); //assert
+        }
 
 
     }
